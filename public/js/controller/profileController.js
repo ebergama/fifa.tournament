3b88cba4e40d29207b1495203ccc737ebda3fa39
Ezequiel Bergamaschi <ebergamaschi@medallia.com>
2015-11-01 02:54:45 -0300

    Fix #27 #28, profile now is mobile friendly.

--- public/js/controller/profileController.js
+++ public/js/controller/profileController.js
@@ -1,17 +1,52 @@
 controllers.controller("profileController", ["$scope", "$location", "player", "matchesForPlayer", "playerStats", "playersData", function($scope, $location, player, matchesForPlayer, playerStats, playersData) {
+    
+    $scope.tabs = ["home", "stats", "history", "feeling"];
+    
+    $scope.selectedTab = "home";
+    
     player.image = player.image.substring(0, player.image.lastIndexOf('?'));
+    
     $scope.thePlayer = player;
+    
     $scope.matchesForPlayer = matchesForPlayer;
+    
     var indexedMatches = _.indexBy(matchesForPlayer, '_id');
     $scope.stats = playerStats;
     $scope.history = _.map(player.rankingHistory || [], function(h) {
         h.match = indexedMatches[h.match];
+        //FIXME: class handling shouldn't be here, duplicated
+        var setResultsClass = function(match) {
+            if (match.home.goals == -1 && match.away.goals == -1) {
+                match.clazz = "notPlayed";
+            } else {
+                var homeGoals = match.home.goals;
+                var awayGoals = match.away.goals;
+                if (homeGoals == awayGoals) {
+                    homeGoals += (match.home.penalties || 0);
+                    awayGoals += (match.away.penalties || 0);
+                } else {
+                    delete match.home.penalties;
+                    delete match.away.penalties;
+                }
+                if (homeGoals > awayGoals) {
+                    match.home.clazz = "won";
+                    match.away.clazz = "";
+                } else if (homeGoals < awayGoals) {
+                    match.home.clazz = "";
+                    match.away.clazz = "won";
+                } else {
+                    match.home.clazz = "";
+                    match.away.clazz = "";
+                }
+            }
+        };
+        setResultsClass(h.match);
         return h;
     });
 
     $scope.getPicture = function(alias) {
         var player = $scope.playersMap[alias];
-        return player ? player.image : "images/icon.question.png";
+        return player ? player.image : null;
     };
 
     $scope.playersMap = {};
@@ -23,6 +58,21 @@ controllers.controller("profileController", ["$scope", "$location", "player", "m
     $scope.goTo = function(player) {
         $location.path('/profile/' + player);
     };
+    
+    $scope.selectTab = function(tab) {
+        $scope.selectedTab = tab;
+    }
+    
+    $scope.getTabName = function(tab) {
+        switch (tab) {
+            case "home": return "Home";
+            case "stats": return "Estadisticas";
+            case "history": return "Historial";
+            case "feeling": return "Feeling";
+
+        }
+        
+    }
 
 
 }]);
